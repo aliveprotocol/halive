b637424a1b2a022a9203e6d9200b899a7d0760fe
techcoderx <engtianxi@outlook.com>
2023-01-27 16:02:34 +0800

    handle sigint and sigterm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import logger from './logger.js'
 import db from './db.js'
 import schema from './schema.js'
+
+let terminating = false
 
 await db.init()
 if (!(await schema.loaded()))
     await schema.setup()
-await db.disconnect()
+
+const handleExit = async () => {
+    if (terminating) return
+    terminating = true
+    process.stdout.write('\r')
+    logger.info('Received SIGINT')
+    await db.disconnect()
+    process.exit(0)
+}
+
+process.on('SIGINT', handleExit)
+process.on('SIGTERM', handleExit)
