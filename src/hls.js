cab978008cd6f3b11a30db60ddc49a391b86e29b
techcoderx <engtianxi@outlook.com>
2023-03-22 15:26:12 +0800

    m3u8 stream api

---
+++ src/hls.js
@@ -0,0 +1,39 @@
+import haliveConfig from './config.js'
+import { MAX_CHUNK_BYTES } from './constants.js'
+import logger from './logger.js'
+
+const hls = {
+    fetchChunk: async (chunk_hash) => {
+        try {
+            // fetch headers to retrieve size
+            // chunk sizes must not exceed MAX_CHUNK_BYTES
+            let controller = new AbortController()
+            const id = setTimeout(() => controller.abort(), haliveConfig.chunk_fetch_timeout*1000)
+            let headChunk = await fetch(haliveConfig.ipfs_gateway+'/ipfs/'+chunk_hash,{ method: 'HEAD', signal: controller.signal })
+            clearTimeout(id)
+            if (headChunk.status !== 200)
+                return { error: 'head chunk failed with status code '+headChunk.status }
+            let chunkSize = headChunk.headers.get('content-length')
+            if (chunkSize > MAX_CHUNK_BYTES)
+                return { error: 'chunk size is greater than MAX_CHUNK_BYTES' }
+            if (headChunk.headers.get('content-type') !== 'text/csv')
+                return { error: 'chunks must be in csv format' }
+
+            // fetch chunk content
+            let controller2 = new AbortController()
+            const id2 = setTimeout(() => controller2.abort(), haliveConfig.chunk_fetch_timeout*1000)
+            let fetchedChunk = await fetch(haliveConfig.ipfs_gateway+'/ipfs/'+chunk_hash,{ method: 'GET', signal: controller2.signal })
+            clearTimeout(id2)
+            if (fetchedChunk.status !== 200)
+                return { error: 'fetch chunk failed with status code '+fetchedChunk.status }
+            return { error: null, chunk: await fetchedChunk.text() }
+        } catch (e) {
+            logger.trace(e)
+            if (e.name === 'AbortError')
+                return { error: 'chunk fetch requet timeout' }
+            return { error: 'unknown error when fetching chunk' }
+        }
+    }
+}
+
+export default hls
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import logger from './logger.js'
 import haliveConfig from './config.js'
 import db from './db.js'
+import hls from './hls.js'
 
 await db.init()
 const app = express()
@@ -23,6 +24,55 @@ app.get('/get_stream_info', async (req,res) => {
         return res.status(404).send({ error: 'stream not found' })
     let stream = streamQuery.rows[0].get_stream_info
     res.send(stream)
+})
+
+app.get('/stream/:author/:link', async (req,res) => {
+    if (typeof req.params.author !== 'string' || typeof req.params.link !== 'string')
+        return res.status(400).send({ error: 'author and/or link is required' })
+    let quality = req.query.quality || 'src'
+    let gw = req.query.gw || haliveConfig.ipfs_gateway
+    let fetchTimeout = parseInt(req.query.fetchtimeout)
+    if (isNaN(fetchTimeout) || fetchTimeout <= 0 || fetchTimeout > haliveConfig.chunk_fetch_timeout)
+        fetchTimeout = haliveConfig.chunk_fetch_timeout
+    let streamQuery = await db.client.query('SELECT halive_api.get_stream_info($1,$2);',[req.params.author,req.params.link])
+    if (streamQuery.rowCount === 0)
+        return res.status(404).send({ error: 'stream not found' })
+    let stream = streamQuery.rows[0].get_stream_info
+    let chunks = await db.client.query('SELECT * FROM halive_api.get_hls_segments($1);',[stream.id])
+
+    // construct m3u8 file
+    let m3u8File = '#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:10\n#EXT-X-MEDIA-SEQUENCE:0'
+    if (stream.ended)
+        m3u8File += '\n#EXT-X-PLAYLIST-TYPE:VOD'
+    else
+        m3u8File += '\n#EXT-X-PLAYLIST-TYPE:EVENT'
+    m3u8File += '\n\n'
+    for (let i in chunks.rows) {
+        if (!chunks.rows[i][quality+'_hash'])
+            continue
+        let chunkLines = []
+        if (!chunks.rows[i].len) {
+            let chunkContents = await hls.fetchChunk(chunks.rows[i][quality+'_hash'])
+            if (chunkContents.error || !chunkContents.chunk)
+                continue
+            chunkLines = chunkContents.chunk.split('\n')
+        } else {
+            // unbundled segments submitted to L1
+            chunkLines = [chunks.rows[i][quality+'_hash']+','+chunks.rows[i].len]
+        }
+        for (let l in chunkLines) {
+            let segment = chunkLines[l].split(',')
+            if (segment.length < 2)
+                continue
+            m3u8File += '#EXTINF:'+segment[1]+',\n'
+            m3u8File += gw+'/ipfs/'+segment[0]+'\n'
+        }
+    }
+    if (stream.ended)
+        m3u8File += '#EXT-X-ENDLIST'
+    // todo fetch from alivedb
+    res.setHeader('Content-Type','text/plain')
+    res.send(m3u8File)
 })
 
 const server = app.listen(haliveConfig.http_port,haliveConfig.http_host,() => {
